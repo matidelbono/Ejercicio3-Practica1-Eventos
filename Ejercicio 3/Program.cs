1fcd4640a49814c98f71110db879150aed57b48a
DAR-LAB-MA\UCSE <a>
2022-05-11 19:18:30 -0300

    Ejercicio 3 completo

---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Timers;
+using logica;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Reloj reloj=new Reloj();
+        reloj.Horario();
+
+    }
+    static void HorarioRealizadoHandler(object? sender, MuestraEventArgs e)
+    {
+        Console.WriteLine("Son las" + e.horario);
+
+    }
+}
+
---
+++ logica/Reloj.cs
@@ -0,0 +1,21 @@
+﻿namespace logica
+{
+    public class Reloj
+    {
+        EventHandler<MuestraEventArgs> HorarioMostrado;
+        public string Hh { get; set; }
+        public string Mm { get; set; }
+        public string ss { get; set; }
+        public string Horario ()
+        {
+            string horario= $"{Hh}: {Mm} :{ss}";
+            if (this.HorarioMostrado!=null)
+            {
+                this.HorarioMostrado(this, new MuestraEventArgs() { horario = horario });
+                return horario;
+            }
+            return "";
+
+    }
+}
+}
